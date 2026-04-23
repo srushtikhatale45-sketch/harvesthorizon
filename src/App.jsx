33169d6330d9f0b1f576be7823a9d7e4e1afca28
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-23 13:14:18 +0530

    final commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import FeaturesPage from './pages/FeaturesPage';
 import DashboardPage from './pages/DashboardPage';
-import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/features" element={<FeaturesPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
