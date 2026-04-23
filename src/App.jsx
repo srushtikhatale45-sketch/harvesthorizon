4bf1d690451f1f3c7435b2ae313eab6249b5cffc
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-23 13:04:33 +0530

    final commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import HomePage from './Pages/HomePage';
+import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import FeaturesPage from './pages/FeaturesPage';
 import DashboardPage from './pages/DashboardPage';
