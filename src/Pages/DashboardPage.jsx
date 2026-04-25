4b42b1b201139164f5d9cd2c3d9e091399aab53f
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-25 22:20:56 +0530

    Final Commit

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -34,25 +34,6 @@ const DashboardPage = () => {
               sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-popups-to-escape-sandbox"
             ></iframe>
           </div>
-          
-          {/* Note about authentication */}
-          <div className="bg-blue-50 border-l-4 border-blue-500 p-4 m-4 rounded">
-            <div className="flex items-start">
-              <div className="flex-shrink-0">
-                <svg className="h-5 w-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                </svg>
-              </div>
-              <div className="ml-3">
-                <p className="text-sm text-blue-700">
-                  You need to be logged into your Power BI account to view the dashboard. 
-                  <a href="#" className="font-semibold underline ml-1 hover:text-blue-800">
-                    Sign in here
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
 
         {/* Alternative: Open in New Window Button */}
